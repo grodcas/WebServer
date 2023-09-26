fdbb9f7aab82c0baa2857724c6be10ab305faadd
grodcas <119622068+grodcas@users.noreply.github.com>
2023-09-26 11:19:14 +0200

    fix bugs

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,9 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = Path(__file__).parent
-CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
@@ -121,22 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)    
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-#STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-
-STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(CORE_DIR, 'media')
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(CORE_DIR, 'mysite/static'),
-)
 
 
 
