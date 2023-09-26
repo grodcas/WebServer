179a8ac0a60fe496a430c53cb3cc80ba40846d1f
grodcas <119622068+grodcas@users.noreply.github.com>
2023-09-26 18:05:34 +0200

    EC2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-wy9e1jk8$!gp=z9ihgs3n60w23%rp6vi&&61f4c2d^xueiqu=1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ginesrguez.pythonanywhere.com","127.0.0.1"]
+ALLOWED_HOSTS = ["ginesrguez.pythonanywhere.com","127.0.0.1",'13.49.57.249:8000']
 
 
 # Application definition
@@ -120,7 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
